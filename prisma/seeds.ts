c3d11fdf2db189d272175da8d4b48348e7f63e47
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-05-30 00:53:08 +0300

    Update seeds.ts

--- prisma/seeds.ts
+++ prisma/seeds.ts
@@ -6,14 +6,14 @@ const prisma = new PrismaClient();
 const initialCategories: Category[] = [
   {
     id: 1,
-    name: 'sell',
-    label: 'Продам',
+    name: 'estate',
+    label: 'Продажа',
     image: 'https://chamala.tatar/uploads/sell.png',
   },
   {
     id: 2,
-    name: 'buy',
-    label: 'Куплю',
+    name: 'rent',
+    label: 'Аренда',
     image: 'https://chamala.tatar/uploads/buy.png',
   },
 ];
